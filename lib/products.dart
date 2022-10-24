26f6f68d7c1876c3c9d48a17ec6895a5120f71af
hakankosum <hakan.kosum@gmail.com>
2022-10-24 14:17:56 +0300

    listview to gridview.

--- lib/products.dart
+++ lib/products.dart
@@ -1,4 +1,5 @@
 import 'dart:core';
+import 'dart:math';
 
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
@@ -42,14 +43,14 @@ class _ProductScreenState extends State<ProductScreen> {
     return Consumer(builder: (BuildContext context, Item item, widget) {
       return Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.black,
+          backgroundColor: Colors.white,
           actions: [
             Container(
                 margin: EdgeInsets.only(right: 5.w),
                 child: Center(
                     child: Text(
                   "Total: " + item.summ.toString(),
-                  style: TextStyle(fontSize: 3.h),
+                  style: TextStyle(fontSize: 3.h,color: Colors.black),
                 )))
           ],
         ),
@@ -59,7 +60,7 @@ class _ProductScreenState extends State<ProductScreen> {
             child: _products.length > 0
                 ? Container(
                     height: 80.h,
-                    child: ListView.builder(
+                    child: GridView.builder(
                       itemCount: _products.length,
                       itemBuilder: (context, index) {
                         return Container(
@@ -67,7 +68,7 @@ class _ProductScreenState extends State<ProductScreen> {
                             padding: EdgeInsets.only(right: 5.w),
                             decoration: BoxDecoration(
                               border: Border.all(color: Colors.grey),
-                              color: Colors.black,
+                              color: Colors.white,
                             ),
                             child: Row(
                               children: [
@@ -89,53 +90,52 @@ class _ProductScreenState extends State<ProductScreen> {
                                 ),
                                 Padding(
                                   padding: const EdgeInsets.all(8.0),
-                                  child: SizedBox(
-                                    height: 4.h,
-                                    
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      
-                                      children: [
-                                        Text(
-                                          _products[index]["id"],
-                                          style: const TextStyle(
-                                              color: Colors.white),
+                                  child: Column(
+                                    children: [
+                                      Spacer(),
+                                      Text(
+                                        _products[index]["id"],
+                                        maxLines: 2,
+                                        style: const TextStyle(
+                                            color: Colors.black,overflow: TextOverflow.fade),
+                                      ),
+                                      Text(
+                                        _products[index]["price"].toString() +
+                                            " TL",
+                                        style: const TextStyle(
+                                            color: Colors.black),
+                                      ),
+                                      Material(
+                                        color: Colors.transparent,
+                                        child: InkWell(
+                                          highlightColor: Colors.black,
+                                          onTap: () {
+                                            item.addBasket(_products[index]);
+                                          },
+                                          child: Container(
+                                              margin: EdgeInsets.only(top: 2.h),
+                                              decoration: BoxDecoration(
+                                                 
+                                                  border: Border.all(),
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          10)),
+                                              child: Icon(
+                                                Icons
+                                                    .add_shopping_cart_outlined,
+                                                size: 24,
+                                                color: Colors.black,
+                                              )),
                                         ),
-                                        Text(
-                                          _products[index]["price"].toString() +
-                                              " TL",
-                                          style: const TextStyle(
-                                              color: Colors.white),
-                                        )
-                                      ],
-                                    ),
+                                      )
+                                    ],
                                   ),
                                 ),
-                                Spacer(),
-                                Material(
-                                  color: Colors.transparent,
-                                  child: InkWell(
-                                    highlightColor: Colors.white,
-                                    onTap: () {
-                                      item.addBasket(_products[index]);
-                                    },
-                                    child: Container(
-                                        padding: EdgeInsets.all(8),
-                                        decoration: BoxDecoration(
-                                            border: Border.all(),
-                                            borderRadius:
-                                                BorderRadius.circular(10)),
-                                        child: Icon(
-                                          Icons.add_shopping_cart_outlined,
-                                          size: 24,
-                                          color: Colors.white,
-                                        )),
-                                  ),
-                                )
                               ],
                             ));
                       },
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2),
                     ),
                   )
                 : null),
@@ -143,7 +143,7 @@ class _ProductScreenState extends State<ProductScreen> {
           height: 10.h,
           width: 100.w,
           child: Container(
-              color: Colors.black,
+              color: Colors.white,
               child: InkWell(
                   onTap: () {
                     Navigator.push(
@@ -156,14 +156,14 @@ class _ProductScreenState extends State<ProductScreen> {
                     children: [
                       (Text(
                         "Go to Basket",
-                        style: TextStyle(fontSize: 24, color: Colors.white),
+                        style: TextStyle(fontSize: 24, color: Colors.black),
                       )),
                       SizedBox(
                         width: 3.w,
                       ),
                       Icon(
                         Icons.shopping_cart_checkout,
-                        color: Colors.white,
+                        color: Colors.black,
                       )
                     ],
                   ))),
